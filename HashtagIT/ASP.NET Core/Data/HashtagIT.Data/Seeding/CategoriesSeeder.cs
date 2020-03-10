b17b9f37155a52045c72ea4ca508fa4c6b478cad
Damian <damian.iv@abv.bg>
2020-03-10 20:33:45 +0200

    Categories Seeder

---
+++ HashtagIT/ASP.NET Core/Data/HashtagIT.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,30 @@
+﻿namespace HashtagIT.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using HashtagIT.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            var categories = new List<string> { "Nature", "Animals", "Popular", "Drone", "Bulgaria", "Food" };
+            foreach (var category in categories)
+            {
+                await dbContext.Categories.AddAsync(new Category
+                {
+                    Name = category,
+                });
+            }
+        }
+    }
+}
